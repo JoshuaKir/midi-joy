fe739082018cc69d066a492c07e3fe806d68b480
JoshuaKir <34132786+JoshuaKir@users.noreply.github.com>
2022-08-12 22:27:58 -0400

    gameManager

--- midi_and_controller/game_inputs.py
+++ midi_and_controller/game_inputs.py
@@ -1,45 +1,69 @@
 import pygame as game
-game.init()
-game.joystick.init()
 
-def get_active_controller():
-    # function returns which controller is currently inputting
-    # This is a qthread so it can run in background
-    # guided by https://realpython.com/python-pyqt-qthread/
-    for event in game.event.get():
-        #print(event)
-        if(event.type == game.JOYBUTTONDOWN or event.type == game.JOYBALLMOTION or event.type == game.JOYHATMOTION):
-            return event.joy
-    
-        elif(event.type == game.JOYAXISMOTION):
-            if(abs(event.value) > 0.1):
-                return event.joy
-        
-        #if no event
-        return -1
+class GameManager:
+    def __init__(self):
+        game.init()
+        game.joystick.init()
 
-def get_active_buttons(controllerID):
-    # function returns which controller is currently inputting
-    # This is a qthread so it can run in background
-    # guided by https://realpython.com/python-pyqt-qthread/
-    #event.get pops items out of the queue so can only be used once
-    #however can continue to check state of buttons and compare from there
-    controller = game.joystick.Joystick(controllerID)
-    activeButtons = []
-    for button in range(controller.get_numbuttons()):
-        #print("what " + str(controller.get_button(button)))
-        if(controller.get_button(button)):
-            activeButtons.append(button)
-            print(activeButtons)
-    
-    return activeButtons
+    def get_active_controllers(self):
+        # function returns which controller is currently inputting
+        # This is a qthread so it can run in background
+        # guided by https://realpython.com/python-pyqt-qthread/
+        activeControllers = []
+        controllerCount = self.get_controllers().get_count()
+        for controllerID in range(controllerCount):
+            controller = self.get_controllers().Joystick(controllerID)
+            for button in range(controller.get_numbuttons()):
+                if(controller.get_button(button)):
+                    activeControllers.append(controller.get_id())
+                    #print(activeButtons)
 
-def get_controller_inputs(controllerID):
-    print(game.joystick.Joystick(controllerID).get_numballs())
-    return[game.joystick.Joystick(controllerID).get_numaxes(), game.joystick.Joystick(controllerID).get_numbuttons()]
+        return activeControllers
 
-def get_controllers():
-    return game.joystick
+    def get_active_buttons(self, controllerID):
+        # function returns which controller is currently inputting
+        # This is a qthread so it can run in background
+        # guided by https://realpython.com/python-pyqt-qthread/
+        #event.get pops items out of the queue so can only be used once
+        #however can continue to check state of buttons and compare from there
+        controller = game.joystick.Joystick(controllerID)
+        activeButtons = []
+        for button in range(controller.get_numbuttons()):
+            #print("what " + str(controller.get_button(button)))
+            if(controller.get_button(button)):
+                activeButtons.append(button)
+                #print(activeButtons)
+
+        return activeButtons
+
+    def get_active_button(self, controllerID):
+        # function returns which controller is currently inputting
+        # This is a qthread so it can run in background
+        # guided by https://realpython.com/python-pyqt-qthread/
+        #need to watch for dropped inputs
+        for event in game.event.get():
+            #can do case and pass in type of input ie axis, button etc
+            if ((event.type == game.JOYBUTTONDOWN or event.type == game.JOYBUTTONUP) and event.joy == controllerID):
+                if (event.type == game.JOYBUTTONDOWN):
+                    return [event.button, True] #tuple for on/off
+
+                elif(event.type == game.JOYBUTTONUP):
+                    return [event.button, False]
+
+                #if no event
+        return [-1, False]
+
+    def run_event_loop(self):
+        #for running loop when no controllerWindows are opened
+        for event in game.event.get():
+            pass
+
+    def get_controller_inputs(self, controllerID):
+        print(game.joystick.Joystick(controllerID).get_numballs())
+        return[game.joystick.Joystick(controllerID).get_numaxes(), game.joystick.Joystick(controllerID).get_numbuttons()]
+
+    def get_controllers(self):
+        return game.joystick
 
 #kinda funny
 #get_power_level
